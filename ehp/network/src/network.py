46e7c0ae138c075d10923c7670ace06b4043c1e2
Ismael Jaras <is.jaras@gmail.com>
2022-09-05 18:41:43 -0300

    include logger and refactor funs to bionet_tools

--- ehp/network/src/network.py
+++ ehp/network/src/network.py
@@ -1,59 +1,15 @@
 import matplotlib.pyplot as plt
 import nest
 import numpy as np
-
 import os
 import sys
 
+from src.logging.logging import logger
+from src.utils.bionet_tools import init_population, connect_pops
 from pynestml.frontend.pynestml_frontend import generate_nest_target
 NEST_SIMULATOR_INSTALL_LOCATION = nest.ll_api.sli_func("statusdict/prefix ::")
 
-
-def init_population(neuron_model: str, n_neurons: int, params: dict,
-                  pos_bounds: list, dim: int):
-    """
-    initialize neuron population
-
-    Parameters
-    ----------
-    neuron_model:
-        neuron model's name
-    n_neurons:
-        how many neurons in that population
-    params:
-        dictionary with parameters for the population
-    pos_bounds:
-        list with min and max random position values
-    dim:
-        spatial dimension
-    """
-    pop_pos = nest.spatial.free(
-                    nest.random.uniform(min=min(pos_bounds),
-                                        max=max(pos_bounds)),
-                    num_dimensions=dim
-                    )
-    pop = nest.Create(neuron_model, n=n_neurons,
-                      positions=pop_pos)
-    for param in params:
-        pop.param = nest.random.normal(mean=param['mean'], std=param['std'])
-    return pop
-
-def connect_pops(pop_pre,
-               pop_post,
-               weights: list):
-    """
-    initialize weights between two populations
-
-    Parameters
-    ----------
-    pop_pre: nest population
-        presynaptic population
-    pop_post: nest population
-        postsynaptic population
-    weights:
-        weights between the two populations
-    """
-    nest.Connect(pop_pre, pop_post)
+logger = logger.getChild(__name__)
 
 
 def connect_noise():
@@ -81,10 +37,47 @@ def init_network(neurons: dict, connections: dict, network_layout: dict,
         external source parameters
     """
     # init pops
-    pop_ex = init_population()
-    pop_in = init_population()
-    # connect both populations
-    connect_pops(pop_ex, pop_in)
+    logger.info("Initializing network")
+    position_dist = network_layout["positions"]["dist"]
+    n_neurons = network_layout["n_neurons"]
+    ex_in_ratio = network_layout["ex_in_ratio"]
+    n_neurons_ex = int(n_neurons * ex_in_ratio)
+    n_neurons_in = n_neurons - n_neurons_ex
+    pop_ex = init_population(position_dist=position_dist,
+                             neuron_model=neurons["ex"]["model"],
+                             n_neurons=n_neurons_ex,
+                             params=neurons["ex"]["params"],
+                             pos_bounds=network_layout["positions"]["pos_bounds"],
+                             dim=network_layout["positions"]["dim"]
+                             )
+    pop_in = init_population(position_dist=position_dist,
+                             neuron_model=neurons["in"]["model"],
+                             n_neurons=n_neurons_in,
+                             params=neurons["in"]["params"],
+                             pos_bounds=network_layout["positions"]["pos_bounds"],
+                             dim=network_layout["positions"]["dim"]
+                             )
+    # Create connections
+    for conn, conn_v in connections.items():
+        print(connections)
+        print(conn)
+        print(conn_v)
+        if conn in ['ex_ex', 'ex_in', 'in_ex', 'in_in']:
+            logger.info("Setting up connection %s -> %s",
+                        conn.split("_")[0], conn.split("_")[1])
+            if conn.split("_")[0] == conn.split("_")[1] == "ex":
+                #connect_pops(pop_ex, pop_ex, conn_v["conn_spec"], conn_v["syn_spec"])
+                pass
+            elif conn.split("_")[0] == conn.split("_")[1] == "in":
+                #connect_pops(pop_in, pop_in, conn_v["conn_spec"], conn_v["syn_spec"])
+                pass
+            elif conn.split("_")[0] == "ex" and conn.split("_")[1] == "in":
+                #connect_pops(pop_ex, pop_in, conn_v["conn_spec"], conn_v["syn_spec"])
+                pass
+            elif conn.split("_")[0] == "in" and conn.split("_")[1] == "ex":
+                #connect_pops(pop_in, pop_ex, conn_v["conn_spec"], conn_v["syn_spec"])
+                pass
+
     # connect external sources
     connect_noise()
     # connect measurement devices
@@ -110,6 +103,7 @@ def run_network(simtime: float, seed: int, record: bool, record_rate: float):
     sim_results: dict
 
     """
-    sim_results = {}
-    nest.Simulate(simtime)
-    return sim_results
+    pass
+    #sim_results = {}
+    #nest.Simulate(simtime)
+    #return sim_results
