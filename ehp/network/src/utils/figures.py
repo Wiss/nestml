fe928f7c627a59aa8710cf90c9b337adbf0cfaee
Ismael Jaras <is.jaras@gmail.com>
2022-09-11 22:47:10 -0300

    include first weights plots. Need to be polish

--- ehp/network/src/utils/figures.py
+++ ehp/network/src/utils/figures.py
@@ -14,8 +14,70 @@ fig_size = (12, 12)
 #NEST_SIMULATOR_INSTALL_LOCATION = nest.ll_api.sli_func("statusdict/prefix ::")
 
 
-def create_weights_figs(weights_evetns: dict, fig_name: str, output_path: str,
+def create_weights_figs(weights_events: dict, fig_name: str, output_path: str,
                       **kargs):
+    # This should be here, it's just a test
+    w = {}
+    valid_pair = []
+    for source, target, time, weight in zip(weights_events['ex_ex']['senders'],
+                                         weights_events['ex_ex']['targets'],
+                                         weights_events['ex_ex']['times'],
+                                         weights_events['ex_ex']['weights']):
+
+        valid_pair.append([source, target])
+        print('source')
+        print(source)
+        print('target')
+        print(target)
+        print('time')
+        print(time)
+        print('weight')
+        print(weight)
+
+        #if source in w.keys():
+       # if not bool(w):
+       #     #if target in w[source].keys():
+       #     if bool(w[source]):
+       #         print(f'{target} key in w[source]')
+       #         w[source][target]['times'].append(time)
+       #         w[source][target]['weights'].append(weight)
+       #     else:
+       #         print(f'no {target} key in w[source]')
+       #         w[source][target] = {'times': [], 'weights': []}
+       # else:
+       #     print(f'no {source} key in w')
+       #     w[source] = dict
+
+        w.setdefault(str(source), {})
+        w[str(source)].setdefault(str(target), {})
+        w[str(source)][str(target)].setdefault('times', []).append(time)
+        w[str(source)][str(target)].setdefault('weights', []).append(weight)
+
+    print(w)
+    plt.figure()
+    sources_set = weights_events['ex_ex']['senders']
+    target_set = weights_events['ex_ex']['targets']
+    #valid_pair_set = set(valid_pair)
+    valid_pair_set = [i for n, i in enumerate(valid_pair) if i not in valid_pair[:n]]
+    for s, t in valid_pair_set:
+        plt.plot(w[str(s)][str(t)]['times'], w[str(s)][str(t)]['weights'], '.', label=f'{s}->{t}')
+        plt.plot(w[str(s)][str(t)]['times'], w[str(s)][str(t)]['weights'])
+    #for s in sources_set:
+     #   for t in target_set:
+      #      try:
+       #         plt.plot(w['3'][t]['times'], w['3'][t]['weights'], label=f'{s}->{t}')
+       #     except:
+        #        pass
+    #plt.plot(w['3']['2']['times'], w['3']['2']['weights'], label=f'{3}->{2}')
+    #plt.plot(w['3']['2']['times'], w['3']['2']['weights'], '.', label=f'{3}->{2}')
+    #plt.plot(w['3']['3']['times'], w['3']['3']['weights'], label=f'{3}->{3}')
+    #plt.plot(w['3']['3']['times'], w['3']['3']['weights'], '.', label=f'{3}->{3}')
+    #plt.plot(w['3']['4']['times'], w['3']['4']['weights'], label=f'{3}->{4}')
+    #plt.plot(w['3']['4']['times'], w['3']['4']['weights'], '.', label=f'{3}->{4}')
+    plt.xlabel('times (ms)')
+    plt.ylabel('weights')
+    plt.legend()
+    plt.show()
     pass
 
 def create_spikes_figs(spikes_events: dict, fig_name: str, output_path: str,
@@ -78,7 +140,7 @@ def create_pops_figs(pop: dict, fig_name: str, output_path: str, **kargs):
     # all together
     fig, ax = plt.subplots(1, figsize=fig_size, sharex=True)
     ax.set_title('Neurons positions')
-    for key, events in pop.items():
+    for key in pop.keys():
         if key == 'ex':
             color = 'r'
         elif key == 'in':
@@ -90,9 +152,9 @@ def create_pops_figs(pop: dict, fig_name: str, output_path: str, **kargs):
                         markersize=pointsize)
             else:
                 ax.plot(p[0], p[1], '.', c=color, markersize=pointsize)
-        ax.set_ylabel('y (mm)', fontsize=fontsize_label)
-        ax.set_xlabel('x (mm)', fontsize=fontsize_label)
-        ax.legend(fontsize=fontsize_legend)
+    ax.set_ylabel('y (mm)', fontsize=fontsize_label)
+    ax.set_xlabel('x (mm)', fontsize=fontsize_label)
+    ax.legend(fontsize=fontsize_legend)
     # save image
     save_spikes_j_fig =f'{output_path}/{fig_name}_joint'
     plt.savefig(save_spikes_j_fig, dpi=500)
